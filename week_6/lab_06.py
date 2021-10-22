a3665aec5cce2f502f56faa9ca62446506f41c7a
herbertlohmus <90004892+herbertlohmus@users.noreply.github.com>
2021-10-21 22:14:44 -0700

    Update lab_06.py

--- week_6/lab_06.py
+++ week_6/lab_06.py
@@ -35,14 +35,31 @@ for tweet in tweets:
     if 'incredible' in tweet['text'].lower():
         num_incredible += 1
 
-print(num_incredible, num_great, num_huge)
-print(len(tweets))
+#print(num_incredible, num_great, num_huge)
+# print(len(tweets))
 print('trump=', num_trumps, 'russia=', num_russia, 'obama=',
       num_obama, 'fake news=', num_fake_news, 'mexico=', num_mexico)
-print('trump=', num_trumps/len(tweets), 'russia=', num_russia/len(tweets), 'obama=',
-      num_obama/len(tweets), 'fake news=', num_fake_news /
-      len(tweets), 'mexico=', num_mexico /
-      len(tweets), 'incredible=', num_incredible/len(tweets),
-      'great=', num_great/len(tweets), 'huge=', num_huge/len(tweets))
+# print('trump=', num_trumps/len(tweets), 'russia=', num_russia/len(tweets), 'obama=',
+#    len(tweets), 'mexico=', num_mexico /
+#    len(tweets), 'incredible=', num_incredible/len(tweets),
+#   'great=', num_great/len(tweets), 'huge=', num_huge/len(tweets))"
 
-txt_trump = "trump:" {}
+#txt_trump = "trump:{:<.2%}".format(num_trumps/len(tweets))
+#txt_russia = "russia:{:<.2%}".format(num_russia/len(tweets))
+#txt_obama = "obama:{:<.2%}".format(num_obama/len(tweets))
+#txt_fake_news = "fake news:{:<.2%}".format(num_fake_news/len(tweets))
+#txt_mexico = "mexico:{:<.2%}".format(num_mexico/len(tweets))
+#txt_incredible = "incredible:{:<.2%}".format(num_incredible/len(tweets))
+#txt_great = "great:{:<.2%}".format(num_great/len(tweets))
+#txt_huge = "huge:{:<.2%}".format(num_huge/len(tweets))
+# print(txt_trump, txt_russia, txt_obama, txt_fake_news,
+#     txt_mexico, txt_incredible, txt_great, txt_huge)
+
+words = [['trump', (num_trumps/len(tweets))], ['russia', num_russia/len(tweets)], ['obama', num_obama/len(tweets)], ['fake news', num_fake_news /
+                                                                                                                     len(tweets)], ['mexico', num_mexico/len(tweets)], ['incredible', num_incredible/len(tweets)], ['great', num_great/len(tweets)], ['huge', num_huge/len(tweets)]]
+
+
+row = "|{country:<16s} | {percentage:.2%}|".format
+print("Percentage of times a word appears in Trump's tweets ")
+for c in words:
+    print(row(country=c[0], percentage=c[1]))
