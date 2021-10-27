a5df4812b519f285a795e139e880e5d342652a00
herbertlohmus <90004892+herbertlohmus@users.noreply.github.com>
2021-10-26 21:42:07 -0700

    Update hw_02.py

--- hw_02/hw_02.py
+++ hw_02/hw_02.py
@@ -1,11 +1,14 @@
+# necessary imports for easy data usage
 import sys
 import csv
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
+
+# increase reading limit
 csv.field_size_limit(sys.maxsize)
 
+# opening of the first data file extraxted from Organize Your Music
 with open('hw_02/spotify_data1.csv', encoding='utf-8') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
@@ -45,15 +48,48 @@ counts = {
     'pop': pop, 'funk': funk, 'disco': disco, 'rap': rap, 'soul': soul, 'classical': classical, 'brass': brass, 'indie': indie, 'rock': rock
 }
 
+# Display exact results
 print('counts=', counts)
 
 xs = sorted(counts.keys())
 ys = [counts[key] for key in xs]
 
+# creating the barchart
 fig, ax = plt.subplots()
 ax.bar(xs, ys)
+
+# formatting the graph
 ax.set_xlabel('Genre')
 ax.set_ylabel('Frequency')
 ax.set_title(
     'How many times does a specific genre appear in my Spotify library')
+
+
+plt.show()
+
+
+## Graph_02 ##
+
+# using the panda framework/library
+data = pd.read_csv('hw_02/valence_dance_energy.csv')
+energy = data['energy']
+dance = data['danceability']
+valence = data['valence']
+
+
+# Creating a scatterplot
+ax = plt.gca()
+
+ax.scatter(energy, dance, color='b')
+ax.scatter(energy, valence, color='r')
+
+
+# formatting the graph
+plt.title('Energy of a song in relation to its Danceability and Valence')
+ax.set_xlabel('Energy')
+ax.set_ylabel('Valence and Danceability')
+plt.legend(["Danceability", "Valence"], loc="upper left")
+
+plt.tight_layout()
+
 plt.show()
